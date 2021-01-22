871f33e1372d56c04574811f0b84a134ca69906a
Adam Cook <CookA@cloud-dfo-mpo.ca>
2021-01-22 00:26:37 +0000

    timings

--- Instructions/RunInParallel.r
+++ Instructions/RunInParallel.r
@@ -2,7 +2,7 @@ require(bio.lobster)
 require(parallel)
 require(lobsterCatch)
 require(devtools)
-load_all('~/git/lobsterCatch')
+load_all('D:/git/lobsterCatch')
 
 arena = matrix(0,200,200)
 y=x=seq(5,195,10)
@@ -29,7 +29,7 @@ p$trapSaturationStart = T
 p$tSteps = 50
 
 
-p$realizations = 100
+p$realizations = 1
 dispersionSaturation = c()
 meanCatchWithSat = c()
 smult_start = seq(.9,1,length.out=8)
@@ -42,4 +42,13 @@ for(i in 1:length(smult_start)){
 	plist[[i]]=pp
 }
 
+#this works in linux
 out = mclapply(X=plist, FUN=SimulateLobsterMovement, mc.cores=length(plist))
+
+#in windows machine
+
+nCores = detectCores()
+cl <- makeCluster(nCores)
+clusterExport(cl,{require(lobsterCatch)
+				  require(bio.lobser)
+				  })
