03fce62a2729649cf2e03417ec0d0e0f5af8154d
Vahab Pourfaraj <73921920+vpourfaraj@users.noreply.github.com>
2021-02-09 15:16:35 -0400

    Update catchability.R

--- R/catchability.R
+++ R/catchability.R
@@ -5,7 +5,7 @@
 #' another lobster entering the trap drops to qmin, the default value is 5
 #' @param Ct is the number of catch
 #' @param r is the instantaneous rate of change in qo with respect to Ct
-#' @return the probability of entry to trap
+#' @return Returns the probability of entry to trap
 #' @export
 catchability<- function(q0= 0.5,qmin=0, saturationThreshold=5, Ct=0, lobSize=NULL){
   
